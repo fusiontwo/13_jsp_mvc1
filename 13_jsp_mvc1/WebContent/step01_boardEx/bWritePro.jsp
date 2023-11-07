673b562f738aed9f5425a031c3fa579b276b88fd
19_web_cyr <19_web_cyr@DESKTOP-R0OB2SQ>
2023-11-07 21:46:27 +0900

    Commit DAO, DTO

---
+++ 13_jsp_mvc1/WebContent/step01_boardEx/bWritePro.jsp
@@ -0,0 +1,43 @@
+<%@page import="step01_boardEx.BoardDAO"%>
+<%@page import="step01_boardEx.BoardDTO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>bWritePro</title>
+</head>
+<body>
+	
+	<% 
+		/*
+			(인코딩)데이터를 받는다.
+			DTO형태로 만든다.
+			DAO객체로 DTO를 보낸다.
+			리액션~!
+		*/
+		
+		request.setCharacterEncoding("utf-8");
+	
+		BoardDTO boardDTO = new BoardDTO();
+		//boardDTO.writer = request.getParameter("writer");
+	
+		boardDTO.setWriter(request.getParameter("writer"));
+		boardDTO.setSubject(request.getParameter("subject"));
+		boardDTO.setEmail(request.getParameter("email"));
+		boardDTO.setPassword(request.getParameter("password"));
+		boardDTO.setContent(request.getParameter("content"));
+		
+		BoardDAO.getInstance().insertBoard(boardDTO);
+	
+	%>
+	
+	<script>
+		alert("게시글이 되었습니다.");
+		// location.href = "url"; > url로 페이지를 이동한다.
+		location.href = "bWrite.jsp";
+	</script>
+
+
+</body>
+</html>
---
+++ 13_jsp_mvc1/src/step01_boardEx/BoardDAO.java
@@ -0,0 +1,73 @@
+package step01_boardEx;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BoardDAO {
+
+	// SingleTon 디자인 패턴
+	private BoardDAO() {}
+	private static BoardDAO instance = new BoardDAO();
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	
+	private Connection conn         = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs            = null;
+	
+	// 데이터 베이스 연결 메서드
+	private void getConnection() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");	
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MVC1_PRACTICE?serverTimezone=Asia/Seoul", "root", "1234");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	// 데이터베이스 연결 해제 메서드
+	private void getClose() {
+		if (rs != null)    try {rs.close();} catch (SQLException e) {e.printStackTrace();}
+		if (pstmt != null) try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+		if (conn != null)  try {conn.close();} catch (SQLException e) {e.printStackTrace();}
+	}
+	
+	
+	// 게시글 추가 DAO
+	public void insertBoard(BoardDTO boardDTO) {
+		
+		// 단위 테스트
+		System.out.println("!!!!!!!!!!!!!!");
+		System.out.println(boardDTO);
+		
+		try {
+			
+			getConnection();
+			
+			String sql = "INSERT INTO BOARD(WRITER , EMAIL , SUBJECT , PASSWORD , CONTENT , READ_CNT , ENROLL_DT)";
+				   sql += "VALUES(?,?,?,?,?,0,NOW())";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardDTO.getWriter());
+			pstmt.setString(2, boardDTO.getEmail());
+			pstmt.setString(3, boardDTO.getSubject());
+			pstmt.setString(4, boardDTO.getPassword());
+			pstmt.setString(5, boardDTO.getContent());
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			getClose();
+		}
+	}
+	
+	
+}
